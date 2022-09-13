3a78887a4eecd766c6884a5e52bb0421d0425662
Atakan Kul <Atakan.Kul@bs.nttdata.com>
2022-09-13 18:29:59 +0300

    Add project files.

---
+++ Services/Product.API/Controllers/ProductController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Product.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Product response";
+        }
+    }
+}
---
+++ Services/User.API/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace User.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "User response";
+        }
+
+        [HttpGet("{id}")]
+        public int Get(int id)
+        {
+            var returnValue = id;
+
+            return returnValue;
+        }
+    }
+}
